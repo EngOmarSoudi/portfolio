558586e24feb673fca1dedeafd91ea1be3e46199
EngOmarSoudi <92035173+EngOmarSoudi@users.noreply.github.com>
2023-02-26 14:01:20 +0300

    nav bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,22 @@ module.exports = {
   content: ["./src/**/*.html"],
   theme: {
     extend: {},
+    container: {
+      center: true,
+    },
+    boxShadow: {
+      cos: "0 0 10px rgb(221, 221, 221)",
+    },
+    colors: {
+      "main-co": "#2196f3",
+      "main-co-l": "#1787e0",
+    },
+    height: {
+      "18": "72px",
+    },
+    textDecoration: {
+      underline: "no-underline",
+    },
   },
   plugins: [],
-}
+};
