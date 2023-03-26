1929f818be991d2c13acd82ccc8a8ed5dffe0403
EngOmarSoudi <92035173+EngOmarSoudi@users.noreply.github.com>
2023-03-27 01:43:25 +0300

    nav bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,9 +12,11 @@ module.exports = {
     colors: {
       "main-co": "#2196f3",
       "main-co-l": "#1787e0",
+      "fe": "#fafafa",
     },
     height: {
       "18": "72px",
+      "1": "4px",
     },
     textDecoration: {
       underline: "no-underline",
